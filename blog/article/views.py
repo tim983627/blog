64000684d90e7b88a70c5a750b9c05876c92f123
tim983627 <tim983627@gmail.com>
2019-11-29 15:26:45 +0800

    ch9 finsh

--- blog/article/views.py
+++ blog/article/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-
+from article.models import Article, Comment
 def article(request):
     '''
     Render the article page
     '''
-    return render(request, 'article/article.html')
+    
+    articles = {article:Comment.objects.filter(article=article) for article in Article.objects.all()}
+    context = {'articles':articles}
+    return render(request, 'article/article.html', context)
