b4b0dca8ecff7a100e2189387b0e06b74db8933c
tim983627 <tim983627@gmail.com>
2019-11-29 16:23:26 +0800

    can post

--- blog/article/views.py
+++ blog/article/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from article.models import Article, Comment
+from article.forms import ArticleForm
 def article(request):
     '''
     Render the article page
@@ -8,3 +9,24 @@ def article(request):
     articles = {article:Comment.objects.filter(article=article) for article in Article.objects.all()}
     context = {'articles':articles}
     return render(request, 'article/article.html', context)
+
+def articleCreate(request):
+    '''
+    Create a new article instance
+        1. If method is GET, render an empty form
+        2. If method is POST,
+           * validate the form and display error messages if the form is invalid
+           * else, save it to the model and redirect to the article page
+    '''
+    template = 'article/articleCreate.html'
+    if request.method == 'GET':
+        print(ArticleForm())
+        return render(request, template, {'articleForm':ArticleForm()})
+
+    # POST
+    articleForm = ArticleForm(request.POST)
+    if not articleForm.is_valid():
+        return render(request, template, {'articleForm':articleForm})
+
+    articleForm.save()
+    return article(request)
