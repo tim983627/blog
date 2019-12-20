489dadf8cf5a7dbb6dac3a44669cbdd60212577a
tim983627 <tim983627@gmail.com>
2019-12-20 16:33:52 +0800

    10 finsh

--- blog/article/views.py
+++ blog/article/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
+from django.db.models.query_utils import Q
 from article.models import Article, Comment
 from article.forms import ArticleForm
 def article(request):
@@ -7,10 +9,11 @@ def article(request):
     '''
     
     articles = {article:Comment.objects.filter(article=article) for article in Article.objects.all()}
-    context = {'articles':articles}
+    context = {'articles':articles} 
     return render(request, 'article/article.html', context)
 
 def articleCreate(request):
+    
     '''
     Create a new article instance
         1. If method is GET, render an empty form
@@ -18,9 +21,9 @@ def articleCreate(request):
            * validate the form and display error messages if the form is invalid
            * else, save it to the model and redirect to the article page
     '''
-    template = 'article/articleCreate.html'
+    
+    template = 'article/articleCreateUpdate.html'
     if request.method == 'GET':
-        print(ArticleForm())
         return render(request, template, {'articleForm':ArticleForm()})
 
     # POST
@@ -29,4 +32,73 @@ def articleCreate(request):
         return render(request, template, {'articleForm':articleForm})
 
     articleForm.save()
-    return article(request)
+    messages.success(request, '文章已新增')
+    return redirect('article:article')
+from django.views.decorators.http import require_POST
+
+...
+
+
+
+def articleRead(request, articleId):
+    '''
+    Read an article
+        1. Get the article instance; redirect to the 404 page if not found
+        2. Render the articleRead template with the article instance and its
+           associated comments
+    '''
+    article = get_object_or_404(Article, id=articleId)
+    context = {
+        'article': article,
+        'comments': Comment.objects.filter(article=article)
+    }
+    return render(request, 'article/articleRead.html', context)
+def articleSearch(request):
+    '''
+    Search for articles:
+        1. Get the "searchTerm" from the HTML form
+        2. Use "searchTerm" for filtering
+    '''
+    searchTerm = request.GET.get('searchTerm')
+    articles = Article.objects.filter(Q(title__icontains=searchTerm) |
+                                      Q(content__icontains=searchTerm))
+    context = {'articles':articles, 'searchTerm':searchTerm} 
+    return render(request, 'article/articleSearch.html', context)
+
+def articleDelete(request, articleId):
+    '''
+    Delete the article instance:
+        1. Render the article page if the method is GET
+        2. Get the article to delete; redirect to 404 if not found
+    '''
+    if request.method == 'GET':
+        return redirect('article:article')
+
+    # POST
+    article = get_object_or_404(Article, id=articleId)
+    article.delete()
+    messages.success(request, '文章已刪除')  
+    return redirect('article:article')
+def articleUpdate(request, articleId):
+    '''
+    Update the article instance:
+        1. Get the article to update; redirect to 404 if not found
+        2. If method is GET, render a bound form
+        3. If method is POST,
+           * validate the form and render a bound form if the form is invalid
+           * else, save it to the model and redirect to the articleRead page
+    '''
+    article = get_object_or_404(Article, id=articleId)
+    template = 'article/articleCreateUpdate.html'
+    if request.method == 'GET':
+        articleForm = ArticleForm(instance=article)
+        return render(request, template, {'articleForm':articleForm})
+
+    # POST
+    articleForm = ArticleForm(request.POST, instance=article)
+    if not articleForm.is_valid():
+        return render(request, template, {'articleForm':articleForm})
+
+    articleForm.save()
+    messages.success(request, '文章已修改') 
+    return redirect('article:articleRead', articleId=articleId)
